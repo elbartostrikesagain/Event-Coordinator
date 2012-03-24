46b4317b9eefdd96c173ee2ed9bfcb438aa70112
Calvin Delamere <calvin.delamere@gmail.com>
2012-03-24 00:09:48 -0400

    use twitter bootstrap for rails flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,12 @@ module ApplicationHelper
   def event_admin?(main_event)
     current_user && main_event.user.id == current_user.id
   end
+  def flash_class(level)
+    case level
+      when :notice then "alert alert-info"
+      when :success then "alert alert-success"
+      when :error then "alert alert-error"
+      when :alert then "alert alert-error"
+    end
+  end
 end
