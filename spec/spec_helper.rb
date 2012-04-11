0d4d014a0a1a565756c108c4b834c55ce4688087
Calvin Delamere <calvin@coshx.com>
2012-04-11 15:45:28 -0600

    color output of rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,6 +10,15 @@ require 'pry'
 Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 
 RSpec.configure do |config|
+  # Use color in STDOUT
+  config.color_enabled = true
+
+  # Use color not only in STDOUT but also in pagers and files
+  config.tty = true
+
+  # Use the specified formatter
+  config.formatter = :progress # :documentation, :progress, :html, :textmate
+
   # ## Mock Framework
   #
   # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
