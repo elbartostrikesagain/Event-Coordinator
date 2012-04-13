87feb2e254b74e555fc35fb053fa03026f655b30
Calvin Delamere <calvin@coshx.com>
2012-04-13 12:09:02 -0600

    move twitter and facebook oauth key/secrets to config vars

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,8 +3,8 @@
 
 #TODO move this out of here since this is a public repo
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, 'hHm7IKlPspkg8t4fZgCKhw', 'MuC5gWUnzYNxWuNOFi099a2K1jXQnSPNXMi5L9JA'
-  provider :facebook, '356355021082665', '9579c10d54db5a9750a71253c03d6b6e'
+  provider :twitter, ENV["TWITTER_KEY"], ENV["TWITTER_SECRET"]
+  provider :facebook, ENV["FACEBOOK_KEY"], ENV["FACEBOOK_SECRET"]
   #provider :openid, nil, :name => 'google_apps', :identifier => 'https://www.google.com/accounts/o8/id'
   #provider :open_id, :name => 'google', :identifier => 'https://www.google.com/accounts/o8/id'
   provider :google, "anonymous", "anonymous"
