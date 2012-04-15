843409d4b4be0db961d28b3dcabca3b86133c9c2
Calvin Delamere <calvin.delamere@gmail.com>
2012-04-14 18:59:29 -0600

    disable calendar for now

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -4,6 +4,9 @@
 class CalendarController < ApplicationController
   def index
     @main_event = MainEvent.find(params[:main_event_id])
+    flash[:error] = "The calendar view has some issues and is down for maintenance when I find time."
+    redirect_to main_event_event_index_path(@main_event)
+    return
     @events_path = main_event_event_path(params[:main_event_id])
     @first_event_date = @main_event.first_event_date
   end
