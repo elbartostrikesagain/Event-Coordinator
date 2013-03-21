0b03dd6b8d6add57a2419e8ad8f62f015c4a571d
Calvin Delamere <calvin.delamere@gmail.com>
2013-03-21 17:45:29 -0600

    precompile bootstrap-wysihtml5.css

--- config/application.rb
+++ config/application.rb
@@ -32,7 +32,7 @@ module EventCoordinator
     # Custom directories with classes and modules you want to be autoloadable.
     # config.autoload_paths += %W(#{config.root}/extras)
     config.autoload_paths += %W(#{config.root}/lib)
-    config.assets.precompile += ['calendar.js', 'fullcalendar.js', 'html_editor.js', 'calendar.js', 'datepicker.js']
+    config.assets.precompile += ['calendar.js', 'fullcalendar.js', 'html_editor.js', 'calendar.js', 'datepicker.js', 'bootstrap-wysihtml5.css']
     config.assets.precompile += %w( *-bundle.js )
 
     # Only load the plugins named here, in the order given (default is alphabetical).
