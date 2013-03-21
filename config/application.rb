8cbeec29295aaa0b8e99898ec4c0843d335aaeef
Calvin Delamere <calvin.delamere@gmail.com>
2013-03-21 15:44:27 -0600

    main_events.js precompiled

--- config/application.rb
+++ config/application.rb
@@ -32,7 +32,7 @@ module EventCoordinator
     # Custom directories with classes and modules you want to be autoloadable.
     # config.autoload_paths += %W(#{config.root}/extras)
     config.autoload_paths += %W(#{config.root}/lib)
-    config.assets.precompile += ['calendar.js', 'fullcalendar.js']
+    config.assets.precompile += ['calendar.js', 'fullcalendar.js', 'main_events.js']
 
 
     # Only load the plugins named here, in the order given (default is alphabetical).
