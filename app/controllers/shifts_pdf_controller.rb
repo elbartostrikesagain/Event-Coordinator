f816027f9e3f3de51d632c0b7ff54310c100fc93
Calvin Delamere <calvin.delamere@gmail.com>
2013-04-05 20:46:51 -0600

    layout false at bottom on controller

--- app/controllers/shifts_pdf_controller.rb
+++ app/controllers/shifts_pdf_controller.rb
@@ -1,7 +1,7 @@
 class ShiftsPdfController < ApplicationController
   def index
-    render :layout => false
     @main_event = MainEvent.find(params[:main_event_id])
     @shifts = @main_event.events
+    render :layout => false
   end
 end
