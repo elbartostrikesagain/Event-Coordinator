fc6d26421eaf61f22be2d31cd17521b70427ccac
Calvin Delamere <calvin.delamere@gmail.com>
2013-04-05 20:58:17 -0600

    order pdf shifts by start date

--- app/controllers/shifts_pdf_controller.rb
+++ app/controllers/shifts_pdf_controller.rb
@@ -1,7 +1,7 @@
 class ShiftsPdfController < ApplicationController
   def index
     @main_event = MainEvent.find(params[:main_event_id])
-    @shifts = @main_event.events
+    @shifts = @main_event.events.order_by([:starts_at, :asc])
     render :layout => false
   end
 end
