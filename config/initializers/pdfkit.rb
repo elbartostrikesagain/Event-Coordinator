f19c5156d6edb2e7a29a8aba8680bc21bf8d5496
Calvin Delamere <calvin.delamere@gmail.com>
2013-04-05 20:34:37 -0600

    try to stop pdf deadlock

--- config/initializers/pdfkit.rb
+++ config/initializers/pdfkit.rb
@@ -1,3 +1,11 @@
 PDFKit.configure do |config|       
   config.wkhtmltopdf = Rails.root.join('bin', 'wkhtmltopdf-amd64').to_s if Rails.env.production?  
-end  
+end
+
+ActionController::Base.asset_host = Proc.new { |source, request|
+  if request.env["REQUEST_PATH"].include? ".pdf"
+    "file://#{Rails.root.join('public')}"
+  else
+    "#{request.protocol}#{request.host_with_port}"
+  end
+}  
