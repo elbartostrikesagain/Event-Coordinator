693353380f766ec6d0c9c555b1f3f9eeb70f8c06
Calvin Delamere <calvin.delamere@gmail.com>
2013-04-24 21:35:27 -0600

    fixes format_event_time_frame for all day events

--- app/helpers/main_events_helper.rb
+++ app/helpers/main_events_helper.rb
@@ -4,7 +4,7 @@ module MainEventsHelper
     if event.all_day == true || event.all_day == "1"
       from = event.starts_at.in_time_zone.strftime("%m/%d/%Y")
       to = event.ends_at.in_time_zone.strftime("%m/%d/%Y")
-      return "All day #{to}" if from.to_date == to.to_date
+      return "All day #{to}" if from == to
       return "#{from} - #{to}"
     elsif event.starts_at.to_date == event.ends_at.to_date
       date = event.starts_at.in_time_zone.strftime("%m/%d/%Y")
