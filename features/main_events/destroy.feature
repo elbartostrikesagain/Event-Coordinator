52273d04d9a049ed9158cf1d7672723a3738e49e
Calvin Delamere <calvin.delamere@gmail.com>
2013-05-03 15:00:31 -0600

    edit test

---
+++ features/main_events/destroy.feature
@@ -0,0 +1,11 @@
+# Feature: Destroy Main Event
+#   Destroying new Main Events
+
+#     Scenario: User destroys a main event
+#       Given I am logged in
+#       And a main event exists
+#       And an event exists
+#       When I follow "Destroy"
+#       And I press "Yes"
+#       Then I should see ""
+#       And I should not see ""
--- features/step_definitions/event_steps.rb
+++ features/step_definitions/event_steps.rb
@@ -2,7 +2,7 @@ Given /^I have created a main event$/ do
   @main_event = FactoryGirl.create(:main_event, user: @current_user)
 end
 
-Given /^A main event exists$/ do
+Given /^a main event exists$/ do
   @main_event = FactoryGirl.create(:main_event)
 end
 
@@ -10,4 +10,12 @@ Given /^A user is registered with the main event$/ do
   @user = FactoryGirl.create(:user)
   @main_event.workers << @user
   @main_event.save!
+end
+
+Then /^I should be editing the main event$/ do
+  page.should have_content "Editing #{@main_event.name}"
+end
+
+Then /^the events name should be "(.*?)"$/ do |name|
+  @main_event.reload.name.should == name
 end
